7ac5906b6c93291bf32ca4b940607876906a1e1b
M Alejandra Rojas M <113424427+alejandra-rojas@users.noreply.github.com>
2024-03-07 16:30:56 +0100

    toggle

---
+++ app/neon-toggle/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+function NeonCursor() {
+  return (
+    <section className="grid place-items-center overflow-hidden h-lvh">
+      <input type="checkbox" role="switch" className="neon" />
+    </section>
+  );
+}
+
+export default NeonCursor;
